9a77ea06a4f7a4151cde0910094e25d65be64a62
longjd <54631877+whatever-however@users.noreply.github.com>
2022-07-06 10:04:43 +0800

    third commit

--- src/main/java/com/atme/git/GitTest.java
+++ src/main/java/com/atme/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atme.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello new git");
+        System.out.println("hello world!");
     }
 }
