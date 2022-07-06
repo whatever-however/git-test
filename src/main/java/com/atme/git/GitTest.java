ccc2a912cddba18d19b66197e62d85c6bc8abb08
longjd <54631877+whatever-however@users.noreply.github.com>
2022-07-06 10:12:53 +0800

    hot fix second commit

--- src/main/java/com/atme/git/GitTest.java
+++ src/main/java/com/atme/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello new git");
         System.out.println("hello world!");
-        System.out.println("custom merge test");
+        System.out.println("conflict merge test");
     }
 }
